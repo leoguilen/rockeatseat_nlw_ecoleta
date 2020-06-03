e38d28384d018b48d438fd87ea558d9faae4a7ed
Leonardo Guilen <leonardo.guilen@treetech.com.br>
2020-06-03 20:40:00 -0300

    Criação do componente da home e para cadastro de um novo ponto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './header'
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handlerClick() {
-    setCounter(counter + 1);
-  }
-
   return (
     <div>
-      <Header title="Hello World" />
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handlerClick}>Aumentar</button>
+      <Routes />
     </div>
   );
 }
