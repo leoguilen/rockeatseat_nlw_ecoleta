ded2385397cde30b164076263b4da54c3386583a
Leonardo Guilen <leonardo.guilen@treetech.com.br>
2020-06-02 21:04:05 -0300

    configuração de conexão para usar o knex com sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json())
+
+app.get('/', (request, response) => {
     response.json({user: "Leonardo"});
 });
 
