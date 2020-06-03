f21d417ee83e8eec8bed0549a52b83afbaea004f
Leonardo Guilen <leonardo.guilen@treetech.com.br>
2020-06-03 19:19:56 -0300

    Limpeza no projeto react, criação do primeiro componente e exemplo de uso do estado e imutabilidade no react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from './header'
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handlerClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-      </header>
+    <div>
+      <Header title="Hello World" />
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={handlerClick}>Aumentar</button>
     </div>
   );
 }
