2d654dc5993db498199e917b3b56c7f7ec6fd773
Leonardo Guilen <leonardo.guilen@treetech.com.br>
2020-06-04 18:35:33 -0300

    Inicio do desenvolvimento do app mobile com react native

---
+++ mobile/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './src/pages/Home/index'
+
+export default function App() {
+  return (
+    <Home />
+  );
+}
