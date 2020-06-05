74c7ef4190315d0a3a8dc945da11f5f8e7303751
Leonardo Guilen <leonardo.guilen@treetech.com.br>
2020-06-04 21:27:13 -0300

    Criação das rotas e navegações entre as pages

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
-import Home from './src/pages/Home/index'
+import Routes from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
     <>
       {/* Componente que muda o estilo da barra de status */}
       <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />    
-      <Home />
+      <Routes />
     </>
   );
 }
