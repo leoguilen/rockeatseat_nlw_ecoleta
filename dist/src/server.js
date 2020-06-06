c5e60062c3386aa74cbd65bf40472119a88591d6
Leonardo Guilen <leonardo.guilen@treetech.com.br>
2020-06-05 21:59:56 -0300

    Construção da branch de prod do server

---
+++ dist/src/server.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var morgan_1 = __importDefault(require("morgan"));
+var path_1 = __importDefault(require("path"));
+var routes_1 = __importDefault(require("./routes"));
+var celebrate_1 = require("celebrate");
+var app = express_1.default();
+var PORT = process.env.PORT || 3333;
+app.use(cors_1.default());
+app.use(morgan_1.default("tiny"));
+app.use(express_1.default.json());
+app.use(routes_1.default);
+app.use('/uploads', express_1.default.static(path_1.default.resolve(__dirname, '..', 'uploads')));
+app.use(celebrate_1.errors());
+app.listen(PORT, function () { console.log('Server is running...'); });
